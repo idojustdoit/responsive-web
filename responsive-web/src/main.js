eb986622a6b7defd106fa0536d8a9c5becb45102
idojustdoit <yulchan9478@gmail.com>
2022-11-09 11:37:08 +0900

    test- commit

--- responsive-web/src/main.js
+++ responsive-web/src/main.js
@@ -1,4 +1,11 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import {
+    createApp
+} from 'vue'
 
-createApp(App).mount('#app')
+import App from './App.vue'
+import store from './store'
+import router from './router'
+
+createApp(App) 
+.use(router)
+.use(store).mount('#app')
